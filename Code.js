475c638921868231d5127ffbfa1148e0082eb407
bplavin <bohdan.plavin@gmail.com>
2020-02-28 15:02:53 -0700

    Html side

--- Code.js
+++ Code.js
@@ -4,5 +4,9 @@ function searchBtn() {
     var btn = document.createElement("BUTTON");
     document.body.appendChild(btn);
   }
+  function searchBtn() {
+    var btn = document.deleteElement("BUTTON");
+    document.body.deleteChild(btn);
+  }
 
   let elem = document.getElementById('elem');
