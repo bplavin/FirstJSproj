cbc6fb9cadbdfb4a3187f026f43d96b318d484af
bplavin <bohdan.plavin@gmail.com>
2020-03-05 16:14:04 -0700

    Closing buttons

--- Code.js
+++ Code.js
@@ -3,9 +3,19 @@
 function createPara() {
     var para = document.createElement("DIV");
     para.innerHTML = "Some note text";
-    document.getElementById("myDIV").appendChild(para).classList.add('TextBox');
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = 'X';
+    para.appendChild(btn);
+    btn.className = "closingBtn";
+    para.className = "paragraphBox";
+    document.getElementById("1").appendChild(para);
   }
   function removePara() {
-    var elem = document.getElementById('myDIV');
+    var elem = document.getElementById('1');
     elem.parentNode.removeChild(elem);
    }
+   function closeParaBox(btn){
+    var close = document.getElementById('1');
+    close.parentElement.removeChild(close);
+   }
+   
