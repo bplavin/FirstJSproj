d89609fbd281cf2c515dbf39436b36f375e07de4
Taras Plavin <tarasplavin@Tarass-MacBook-Air.local>
2020-03-14 11:39:40 -0700

    preview changes

--- Code.js
+++ Code.js
@@ -4,31 +4,61 @@
   F1 -> format document
 */
 
+const NOTE_BOX = "paragraphBox";
+
+let counter = 0;
+
 function createPara() {
   /* 1) it is note, not para 
       2) each button inside div should have onClick event which you can use to remove element from DOM
       https://stackoverflow.com/questions/19998711/how-to-pass-this-element-to-javascript-onclick-function-and-add-a-class-to-that
       https://stackoverflow.com/questions/8830839/javascript-dom-remove-element
   */
-  var para = document.createElement("DIV");
+
+  counter ++;
+  let para = document.createElement("DIV");
+  para.className = NOTE_BOX;
   para.innerHTML = "Some note text";
-  var btn = document.createElement("BUTTON");
+  para.id = counter;
+
+  let btn = document.createElement("BUTTON");
   btn.innerHTML = 'X';
-  para.appendChild(btn);
   btn.className = "closingBtn";
-  para.className = "paragraphBox";
-  document.getElementById("1").appendChild(para);
+  btn.onclick = closeParaBox;
+  
+  para.appendChild(btn);
+  document.getElementById("note-container").appendChild(para);
 }
 
 function removePara() {
   /* here you remove parent for your notes
     when I press add add add -> then clear all -> then add it doesn't work
   */
-  var elem = document.getElementById('1');
-  elem.parentNode.removeChild(elem);
+  let elem = document.getElementById('note-container');
+  let notes = document.getElementsByClassName(NOTE_BOX);
+  let temp = [];
+  for (var i = 0; i < notes.length; i++) {
+      temp.push(notes[i]);
+  }
+
+  for (var i = 0; i < temp.length; i++) {
+      elem.removeChild(temp[i]);
+  }
 }
 
-function closeParaBox(btn) {
-  var close = document.getElementById('1');
-  close.parentElement.removeChild(close);
+function closeParaBox(event) {
+  console.log(event);
+  var btn = event.target;
+  const note = btn.parentNode;
+  const mainContainer = note.parentNode;
+  mainContainer.removeChild(note);
 }
+
+function closeParaBox(event) {
+  console.log(event);
+  var btn = event.target;
+  const note = btn.parentNode;
+  const mainContainer = note.parentNode;
+  mainContainer.removeChild(note);
+}
+
