ee89b00362f04814b7c5e24d480bb7464b484d05
bplavin <bohdan.plavin@gmail.com>
2020-06-21 16:11:26 -0700

    pagination#1

--- Code.js
+++ Code.js
@@ -1,7 +1,12 @@
 "use strict";
-
 let data = [];
 let usersData = [];
+let arrOfId = [];
+
+let currentPage = 1;
+let numberPerPage = 20;
+
+
 const NOTE_BOX = "paragraphBox";
 
 fetch('https://jsonplaceholder.typicode.com/todos')
@@ -11,6 +16,8 @@ fetch('https://jsonplaceholder.typicode.com/todos')
             data.push(commit);
             createNote(commit);
         }
+        paginationButtons(commits);
+        displayNotes(commits);
     });
 
 fetch('https://jsonplaceholder.typicode.com/users')
@@ -22,15 +29,17 @@ fetch('https://jsonplaceholder.typicode.com/users')
         }
     });
 
-function createUsersCheckBox(userName) {
+function createUsersCheckBox(user) {
     let filterContainer = document.createElement('div');
     filterContainer.className = "filter";
 
     let checkbox = document.createElement('input');
     checkbox.type = 'checkbox';
+    checkbox.id = user.id;
+    checkbox.checked = true;
 
     let label = document.createElement('label');
-    label.innerText = userName.name;
+    label.innerText = user.name;
 
     filterContainer.appendChild(checkbox);
     filterContainer.appendChild(label);
@@ -40,7 +49,8 @@ function createUsersCheckBox(userName) {
 function createNote(oneNote) {
     let paragraph = document.createElement("DIV");
     paragraph.className = NOTE_BOX;
-    paragraph.innerText = oneNote.title;
+    paragraph.innerText = oneNote.title + oneNote.userId;
+
 
     let btn = document.createElement("BUTTON");
     btn.innerHTML = 'X';
@@ -51,29 +61,104 @@ function createNote(oneNote) {
     document.getElementById("note-container").appendChild(paragraph);
 }
 
-function filterNotes(selectObject) {
+function filterNotes(filterElement) {
     let noteContainer = document.getElementById('note-container');
     let notes = document.getElementsByClassName(NOTE_BOX);
+    let temp = [];
+
     for (let i = 0; i < notes.length; i++) {
-        noteContainer.removeChild(notes[i]);
+        temp.push(notes[i]);
     }
 
-    let value = selectObject.value;
+    for (let i = 0; i < temp.length; i++) {
+        noteContainer.removeChild(temp[i])
+    }
+
+    let value = filterElement.value;
+    let selectedUseres = getUsersId();
+
+
+    function getUsersId() {
+        let checkData = [];
+        for (let user of usersData) {
+            let checkbox = document.getElementById(user.id + '');
+            if (checkbox.checked == true) {
+                checkData.push(user.id);
+            }
+        }
+        return checkData;
+    };
+
 
     for (let note of data) {
-        if (value == "allNotes") {
+        if (value == "allNotes" && selectedUseres.includes(note.userId)) {
             createNote(note);
-        } else if (value == "done_notes" && note.completed) {
+        } else if (value == "done_notes" && selectedUseres.includes(note.userId) && note.completed) {
             createNote(note);
-        } else if (value == 'pending_notes' && !note.completed) {
+        } else if (value == 'pending_notes' && selectedUseres.includes(note.userId) && !note.completed) {
             createNote(note);
         }
-    }
-}
+    };
+
+};
 
 function closeParaBox(event) {
     let btn = event.target;
     const note = btn.parentNode;
     const mainContainer = note.parentNode;
     mainContainer.removeChild(note);
-}
+};
+
+function displayNotes(items) {
+    let noteContainer = document.getElementById('note-container');
+    let notes = document.getElementsByClassName(NOTE_BOX);
+    let temp = [];
+
+    for (let i = 0; i < notes.length; i++) {
+        temp.push(notes[i]);
+    }
+
+    for (let i = 0; i < temp.length; i++) {
+        noteContainer.removeChild(temp[i])
+    }
+
+
+    let begin = ((currentPage - 1) * numberPerPage);
+    let end = begin + numberPerPage;
+    let pageList = items.slice(begin, end);
+
+    for (let i = 0; i < pageList.length; i++) {
+        let item = pageList[i];
+        createNote(item);
+    }
+};
+
+
+function paginationButtons(list, items) {
+    let buttons = document.getElementById('pagination-container');
+
+    let page_count = Math.ceil(list.length / numberPerPage);
+    for (let i = 1; i < page_count + 1; i++) {
+        let btn = createButtons(i, items);
+        buttons.appendChild(btn);
+    }
+};
+
+function createButtons(page, items) {
+    let button = document.createElement('button');
+    button.innerText = page;
+
+    if (currentPage == page) button.classList.add('active');
+
+    button.addEventListener('click', function () {
+        currentPage = page;
+        displayNotes(items, numberPerPage, currentPage);
+
+        let current_btn = document.querySelector('.pagenumbers button.active');
+        current_btn.classList.remove('active');
+
+        button.classList.add('active');
+        });
+        
+    return button;
+};
